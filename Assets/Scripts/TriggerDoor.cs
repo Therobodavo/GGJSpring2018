bc666c5654b611a5148a39a76b93fbef7d51e2ff
David Knolls <Therobodavo@gmail.com>
2018-01-27 11:23:55 -0500

    Added a door to finish/teleport from

---
+++ Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = new Vector3(0f,0f,0f);
+        Debug.Log("ds");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //Do whatever you want
+    }
+}
