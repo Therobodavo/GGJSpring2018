a84b1bce09f634cafe9669a2d00dd8a25d29b60e
David Knolls <Therobodavo@gmail.com>
2018-01-27 22:03:16 -0500

    Added Music

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Awake()
+    {
+        if(GameObject.FindGameObjectsWithTag("MenuMusic").Length > 0)
+        {
+            Destroy(GameObject.Find("MenuMusic"));
+        }  
+        if(GameObject.FindGameObjectsWithTag("GameMusic").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Awake()
+    {
+        if(GameObject.FindGameObjectsWithTag("GameMusic").Length > 0)
+        {
+            Destroy(GameObject.Find("GameMusic"));
+        }  
+        if(GameObject.FindGameObjectsWithTag("MenuMusic").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
